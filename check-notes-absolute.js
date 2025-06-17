dd9f41a3a56f005a4d4615c56a563d482c9a371a
Thea Holtlund Jacobsen <117310317+theaholtlund@users.noreply.github.com>
2025-06-17 17:08:18 +0200

    Add main logic for absolute search

--- check-notes-absolute.js
+++ check-notes-absolute.js
@@ -1,5 +1,47 @@
 #!/usr/bin/env osascript -l JavaScript
 
-const cal = Application("Calendar");
+ObjC.import("stdlib");
+ObjC.import("Foundation");
 
-const calendarName = $.getenv("CALENDAR_NAME");
+const app = Application.currentApplication();
+app.includeStandardAdditions = true;
+
+function loadEnv(path) {
+  const fm = $.NSFileManager.defaultManager;
+
+  const content = $.NSString.stringWithContentsOfFileEncodingError(
+    path,
+    $.NSUTF8StringEncoding,
+    null
+  );
+
+  const lines = ObjC.unwrap(content).split("\n");
+  const env = {};
+  lines.forEach((line) => {
+    line = line.trim();
+    const [key, ...vals] = line.split("=");
+    const keyTrimmed = key.trim();
+    const valueRaw = vals.join("=");
+    env[keyTrimmed] = valueRaw;
+  });
+  return env;
+}
+
+const cwd = $.getenv("PWD");
+const envPath = `${cwd}/.env`;
+const env = loadEnv(envPath);
+
+const calendarName = env.CALENDAR_NAME;
+const searchText = env.CALENDAR_SEARCH_TEXT;
+
+// Compose path for AppleScript
+const scriptPath = `${cwd}/apple-scripts/search-notes-absolute.scpt`;
+
+const out = app.doShellScript(
+  `CALENDAR_NAME="${calendarName}" CALENDAR_SEARCH_TEXT="${searchText}" osascript "${scriptPath}"`
+);
+
+out.split("\n").forEach((line) => {
+  const [title, dateStr] = line.split("|||");
+  console.log(`✔ "${title}" @ ${dateStr}`);
+});
